1e14e65a1038fc8d12529438c5a58311bdbcc3b7
황정주 <hjj950303@naver.com>
2023-01-18 03:38:00 +0900

    feat: make spring-boot project basic, signup, login, logout

---
+++ bbb/src/main/java/com/ssafy/bbb/db/domain/Room.java
@@ -0,0 +1,31 @@
+package com.ssafy.bbb.db.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED) //모든 필드 값을 파라미터로 받는 생성자를 만듦
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //기본생성자 생성
+public class Room implements Serializable {
+
+    //pk
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Auto Increment 지원
+    private Long id;
+
+    //생성일시
+    @Column(name = "creation_date", columnDefinition = "DEFAULT CURRENT_TIMESTAMP")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creationDate;
+
+    //상태("OFF" : 존재하지 않는 방, "ON" : 입장 가능한 방, "GAME" : 게임 진행 중)
+    @Column(length = 10)
+    private String status;
+}
