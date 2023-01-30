5cc3ba2ff045d70a62900c0c67ba34e2dc6533e7
sonmh79 <sonmh79@naver.com>
2023-01-30 13:27:01 +0900

    refactor: add comment and Serializable on Word Entity

--- backend/src/main/java/com/bbb/pjtname/db/domain/Word.java
+++ backend/src/main/java/com/bbb/pjtname/db/domain/Word.java
@@ -6,23 +6,28 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Getter //getter 생성
 @AllArgsConstructor //모든 필드 값을 파라미터로 받는 생성자를 만듦
 @NoArgsConstructor(access = AccessLevel.PROTECTED) //기본생성자 생성
-public class Word {
+public class Word implements Serializable {
 
+    // PK
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //Auto Increment 지원
     private Long id;
 
+    // 카테고리 이름
     @Column(length = 20, nullable = false)
     private String name;
 
+    // content url
     @Column(length = 200, name = "content_url", nullable = false)
     private String contentUrl;
 
+    // FK - 카테고리
     @ManyToOne
     @JoinColumn(name = "category_id", nullable = false)
     private Category category;
