ac1b5d534f8ba7159d563501fe6c92c8b4b18770
Youngmook Lim <youngmookk@gmail.com>
2023-01-30 13:04:36 +0900

    feat: add builder annotation to gamelog and gamelogUser

--- backend/src/main/java/com/bbb/pjtname/db/domain/GamelogUser.java
+++ backend/src/main/java/com/bbb/pjtname/db/domain/GamelogUser.java
@@ -1,15 +1,13 @@
 package com.bbb.pjtname.db.domain;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
 @Getter //getter 생성
+@Builder
 @AllArgsConstructor //모든 필드 값을 파라미터로 받는 생성자를 만듦
 @NoArgsConstructor(access = AccessLevel.PROTECTED) //기본생성자 생성
 @Table(name = "gamelog_user")
