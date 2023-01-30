4055fd96120eb0ac6ed8bb549ce48aa2e1c7be79
Youngmook Lim <youngmookk@gmail.com>
2023-01-30 13:10:54 +0900

    feat: add gamelog save to database functionality

--- backend/src/main/java/com/bbb/pjtname/api/service/GameService.java
+++ backend/src/main/java/com/bbb/pjtname/api/service/GameService.java
@@ -2,9 +2,9 @@ package com.bbb.pjtname.api.service;
 
 import com.bbb.pjtname.api.request.GameRemoveUserReq;
 import com.bbb.pjtname.db.domain.Gamelog;
-import com.bbb.pjtname.db.domain.GamelogUser;
 import com.bbb.pjtname.db.repository.GamelogRepository;
 import com.bbb.pjtname.db.repository.GamelogUserRepository;
+import com.bbb.pjtname.db.repository.UserRepository;
 import com.bbb.pjtname.exception.NoConnectionError;
 import com.bbb.pjtname.exception.NotFoundException;
 import com.bbb.pjtname.exception.RoomOverflowException;
@@ -28,6 +28,7 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class GameService {
     private final GamelogRepository gamelogRepository;
     private final GamelogUserRepository gamelogUserRepository;
+    private final UserRepository userRepository;
 
     // OpenVidu 서버 URL 및 암호키(BBB) 설정 (application.properties에서 가져옴)
     @Value("${OPENVIDU_URL}")
@@ -76,7 +77,7 @@ public class GameService {
         gameRooms = new ConcurrentHashMap<>();
     }
 
-    public Map<String, Object> enterRoom() throws OpenViduJavaClientException, OpenViduHttpException {
+    public Map<String, Object> enterRoom(Long userId) throws OpenViduJavaClientException, OpenViduHttpException {
 
         Connection connection = null;
         String token = null;
@@ -101,8 +102,13 @@ public class GameService {
             throw new RoomOverflowException();
         }
 
+        // connection의 params 만들기 : 해당 connection에 연결되는 유저의 ID값 저장
+        ConnectionProperties connectionProperties = new ConnectionProperties.Builder()
+                .data(userId.toString())
+                .build();
+
         // 큐의 맨 앞에 있는 대기방의 참가자 수가 4 미만일 경우 connection 생성
-        connection = availableSession.createConnection();
+        connection = availableSession.createConnection(connectionProperties);
 
         if (connection == null) {
             throw new NoConnectionError();
@@ -133,11 +139,12 @@ public class GameService {
             Map<String, Object> sessionInfo = gameRooms.get(id);
 
             Session session = (Session) sessionInfo.get("session");
-            LocalDateTime startDate = (LocalDateTime) sessionInfo.get("startDate");
+
 
             if (session == null) {
                 throw new NotFoundException("세션을 찾을 수 없습니다.");
             }
+
 
             // 해당 세션에 연결된 모든 connection 퇴출
             session.close();
@@ -151,9 +158,25 @@ public class GameService {
             gameRooms.remove(id);
             standbyRooms.add(session);
 
-            // DB에 게임 로그 저장*****************
-            Gamelog gamelog = new Gamelog();
-            GamelogUser gamelogUser = new GamelogUser();
+            // DB 저장 //////////////////////////////////////
+            // DB에 게임 로그(gamelog) 데이터 저장
+
+            LocalDateTime startDate = (LocalDateTime) sessionInfo.get("startDate");
+            LocalDateTime endDate = LocalDateTime.now();
+            String sessionId = session.getSessionId();
+
+            Gamelog gamelog = Gamelog.builder()
+                    .startDate(startDate)
+                    .endDate(endDate)
+                    .sessionId(sessionId)
+                    .build();
+
+            gamelogRepository.save(gamelog);
+
+            // DB에 로그-회원(gamelog_user) 데이터 저장
+
+
+//            User user = userRepository.findById();
 
 
             return 0;
