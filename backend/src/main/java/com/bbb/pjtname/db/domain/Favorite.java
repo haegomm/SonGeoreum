9185071c5f83426edb4ba5187721ba0abf28f27b
손민혁 <sonmh79@naver.com>
2023-01-31 04:07:13 +0900

    feat: add ToString annotation for debugging

--- backend/src/main/java/com/bbb/pjtname/db/domain/Favorite.java
+++ backend/src/main/java/com/bbb/pjtname/db/domain/Favorite.java
@@ -1,9 +1,6 @@
 package com.bbb.pjtname.db.domain;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -13,6 +10,7 @@ import java.time.LocalDateTime;
 @Getter //getter 생성
 @AllArgsConstructor //모든 필드 값을 파라미터로 받는 생성자를 만듦
 @NoArgsConstructor(access = AccessLevel.PROTECTED) //기본생성자 생성
+@ToString
 public class Favorite implements Serializable {
 
     // PK
