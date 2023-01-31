0a751345878bd0b8a98f24683b7a16e11aec2177
sonmh79 <sonmh79@naver.com>
2023-01-31 09:36:36 +0900

    refactor: add space

--- backend/src/main/java/com/bbb/pjtname/api/service/CategoryService.java
+++ backend/src/main/java/com/bbb/pjtname/api/service/CategoryService.java
@@ -18,8 +18,10 @@ public class CategoryService {
     private final CategoryRepository categoryRepository;
 
     public List<CategoryRes> findAllCategory() {
+
         List<Category> categoryList = categoryRepository.findAll();
         List<CategoryRes> categoryResList = categoryList.stream().map(category -> CategoryRes.builder().category(category).build()).collect(Collectors.toList());
+
         return categoryResList;
     }
 }
