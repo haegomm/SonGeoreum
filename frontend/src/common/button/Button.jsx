f364c37e7cd1f8741a144bf12581f5c068148e5a
seohyeonkyeong <hkyung99@naver.com>
2023-02-03 01:03:41 +0900

    feat: custom testStart button component

--- frontend/src/common/button/Button.jsx
+++ frontend/src/common/button/Button.jsx
@@ -2,20 +2,20 @@ import Button from "@mui/material/Button";
 
 // button customizing
 // 0: default (기본), 1: next (다음), 2: testModeSelect (테스트모드선택)
-// 3: learnToTest (학습하기->테스트)
+// 3: learnToTest (학습하기->테스트), 4: testStart (테스트시작)
 
 const typeNameList = [
   "default",
   "next",
   "testModeSelect",
   "learnToTest",
+  "testStart",
   "webcamTest",
 ];
 const typeList = [
   {
     // 0: default 버튼
     variant: "contained",
-    href: "",
     width: 200,
     height: 50,
     borderRadius: 20,
@@ -26,7 +26,6 @@ const typeList = [
   {
     // 1: next 버튼
     variant: "contained",
-    href: "",
     width: 192,
     height: 72,
     borderRadius: 10,
@@ -37,7 +36,6 @@ const typeList = [
   {
     // 2: testModeSelect 버튼
     variant: "contained",
-    href: "",
     width: 256,
     height: 108,
     borderRadius: 20,
@@ -48,18 +46,26 @@ const typeList = [
   {
     // 3: learnToTest 버튼
     variant: "contained",
-    href: "",
     width: 216,
     height: 64,
     borderRadius: 10,
-    fontSize: 32,
+    fontSize: 40,
+    fontWeight: 500,
+    color: "white",
+  },
+  {
+    // 4: testStart 버튼
+    variant: "contained",
+    width: 192,
+    height: 72,
+    borderRadius: 10,
+    fontSize: 40,
     fontWeight: 500,
     color: "white",
   },
   {
     // 9: webcamTest 버튼
     variant: "contained",
-    href: "",
     width: 336,
     height: 88,
     borderRadius: 90,
@@ -69,9 +75,16 @@ const typeList = [
   },
 ];
 
-export default function LargeButton({ text, type, backgroundColor, disable }) {
-  console.log({ text, type, backgroundColor, disable });
+export default function LargeButton({
+  text,
+  type,
+  backgroundColor,
+  disable,
+  onclick,
+}) {
+  console.log({ text, type, backgroundColor, disable, onclick });
   const disabled = disable ? disable : false;
+  const onclickMethod = onclick ? onclick : undefined;
 
   let selectedType = typeList[0];
   for (let i = 0; i < typeNameList.length; i++) {
@@ -84,7 +97,7 @@ export default function LargeButton({ text, type, backgroundColor, disable }) {
   return (
     <Button
       variant={selectedType.variant}
-      href={selectedType.href}
+      onClick={onclickMethod}
       color={backgroundColor}
       disabled={disabled}
       style={{
