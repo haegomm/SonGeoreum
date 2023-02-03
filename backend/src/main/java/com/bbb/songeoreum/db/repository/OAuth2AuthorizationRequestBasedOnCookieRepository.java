e02f701b03a6a3e05a9f708f7176cd25ea730a7c
jeongjuhwang <hjj950303@naver.com>
2023-02-03 16:46:48 +0900

    refactor: add Component annotation to OAuth2AuthorizationRequestBasedOnCookieRepository

--- backend/src/main/java/com/bbb/songeoreum/db/repository/OAuth2AuthorizationRequestBasedOnCookieRepository.java
+++ backend/src/main/java/com/bbb/songeoreum/db/repository/OAuth2AuthorizationRequestBasedOnCookieRepository.java
@@ -4,10 +4,12 @@ import com.bbb.songeoreum.util.CookieUtil;
 import com.nimbusds.oauth2.sdk.util.StringUtils;
 import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Component
 public class OAuth2AuthorizationRequestBasedOnCookieRepository implements AuthorizationRequestRepository<OAuth2AuthorizationRequest> {
 
     public final static String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME = "oauth2_auth_request";
