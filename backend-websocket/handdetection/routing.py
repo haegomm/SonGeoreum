8e47d89e81ba234fd284c719a5ed2290ee9cf893
sonmh79 <sonmh79@naver.com>
2023-02-03 17:26:06 +0900

    feat: add parameter on websocket url

--- backend-websocket/handdetection/routing.py
+++ backend-websocket/handdetection/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/socket-server',consumers.HanddetectionConsumer.as_asgi())
+    re_path(r'^ws/socket-server/(?P<category>\w+)',consumers.HanddetectionConsumer.as_asgi())
 ]
