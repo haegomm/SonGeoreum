c9bb93ef169ea449c0ee065978437ce9da6264c2
손민혁 <sonmh79@naver.com>
2023-02-06 03:15:46 +0900

    refactor: add blank lines after import and remove redundant parentheses

--- backend-websocket/handdetection/knn.py
+++ backend-websocket/handdetection/knn.py
@@ -1,20 +1,23 @@
 import cv2
 import numpy as np
 
-class Knn():
+
+class Knn:
     def __init__(self, category):
         self.knn = []
         self.gesture = []
 
         if category == 'consonant':
             file = np.genfromtxt('knn_dataset_consonant.txt', delimiter=",")
-            self.gesture = {0: 'ㄴ', 1: 'ㄷ', 2: 'ㄹ', 3: 'ㅁ', 4: 'ㅂ', 5: 'ㅅ', 6: 'ㅇ', 7: 'ㅈ', 8: 'ㅊ', 9: 'ㅋ', 10: "ㅌ", 11: 'ㅍ', 12: 'ㅎ', 13: "ㄱ"}
+            self.gesture = {0: 'ㄴ', 1: 'ㄷ', 2: 'ㄹ', 3: 'ㅁ', 4: 'ㅂ', 5: 'ㅅ', 6: 'ㅇ', 7: 'ㅈ', 8: 'ㅊ', 9: 'ㅋ', 10: "ㅌ",
+                            11: 'ㅍ', 12: 'ㅎ', 13: "ㄱ"}
         elif category == 'vowel':
             file = np.genfromtxt('knn_dataset_vowel.txt', delimiter=",")
-            self.gesture = {0:'ㅏ',1:'ㅑ',2:'ㅓ',3:'ㅕ',4:'ㅗ',5:'',6:'ㅜ',7:'ㅠ',8:'ㅡ',9:'ㅣ',10:"ㅐ",11:'ㅒ',12:'ㅔ',13:"ㅖ"}
+            self.gesture = {0: 'ㅏ', 1: 'ㅑ', 2: 'ㅓ', 3: 'ㅕ', 4: 'ㅗ', 5: '', 6: 'ㅜ', 7: 'ㅠ', 8: 'ㅡ', 9: 'ㅣ', 10: "ㅐ",
+                            11: 'ㅒ', 12: 'ㅔ', 13: "ㅖ"}
         elif category == 'number':
             file = np.genfromtxt('knn_dataset_number.txt', delimiter=",")
-            self.gesture = {0:'0', 1:'1', 2:'2', 3:'3', 4:'4', 5:'5', 6:'6', 7:'7', 8:'8', 9:'9'}
+            self.gesture = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9'}
 
         angle_file = file[1:, :-1]
         label_file = file[1:, -1]
