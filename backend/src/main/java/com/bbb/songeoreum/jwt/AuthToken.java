50ad68a73c132148e29f697d6004105499c04c3e
황정주 <hjj950303@naver.com>
2023-02-06 02:01:45 +0900

    fix: fix AuthToken, AuthTokenProvider bugs, add TokenValidFailedException

--- backend/src/main/java/com/bbb/songeoreum/jwt/AuthToken.java
+++ backend/src/main/java/com/bbb/songeoreum/jwt/AuthToken.java
@@ -19,80 +19,99 @@ public class AuthToken { // JwtUtil
     private static final String AUTHORITIES_KEY = "role";
 
     // refresh token
-    public AuthToken(String id, Date expiry, Key key) {
+    public AuthToken(Long id, Date expiry, Key key) {
         this.key = key;
         this.token = createAuthToken(id, expiry);
     }
 
     // access token
-    public AuthToken(String id, String role, Date expiry, Key key) {
+    public AuthToken(Long id, String role, Date expiry, Key key) {
         this.key = key;
         this.token = createAuthToken(id, role, expiry);
     }
 
+    /**
+     * key : Claim에 셋팅될 key 값
+     * data : Claim에 셋팅 될 data 값
+     * subject : payload에 sub의 value로 들어갈 subject값
+     * expire : 토큰 유효기간 설정을 위한 값
+     * jwt 토큰의 구성 : header+payload+signature
+     */
     // jwt refresh token 생성
-    private String createAuthToken(String id, Date expiry) {
+    private String createAuthToken(Long id, Date expiry) {
         return Jwts.builder()
-                .setSubject(id)
-                .signWith(SignatureAlgorithm.HS256, key)
+                // Header 설정 : 토큰의 타입, 해쉬 알고리즘 정보 세팅.
+                .setHeaderParam("type", "JWT")
+                .setHeaderParam("createdDate", System.currentTimeMillis()) // 생성 시간
+                // Payload 설정 : 유효기간(Expiration), 토큰 제목 (Subject), 데이터 (Claim) 등 정보 세팅.
+                .setSubject("refreshToken") // 토큰 제목 설정 ex) accessToken, refreshToken
+                .claim("id", id) // user table PK
+                // Signature 설정 : secret key를 활용한 암호화.
+                .signWith(key, SignatureAlgorithm.HS256)
                 .setExpiration(expiry)
                 .compact();
     }
 
     // jwt access token 생성
-    private String createAuthToken(String id, String role, Date expiry) {
+    private String createAuthToken(Long id, String role, Date expiry) {
         return Jwts.builder()
-                .setSubject(id)
+                // Header 설정 : 토큰의 타입, 해쉬 알고리즘 정보 세팅.
+                .setHeaderParam("type", "JWT")
+                .setHeaderParam("createdDate", System.currentTimeMillis()) // 생성 시간
+                // Payload 설정 : 유효기간(Expiration), 토큰 제목 (Subject), 데이터 (Claim) 등 정보 세팅.
+                .setSubject("accessToken")
                 .claim(AUTHORITIES_KEY, role)
-                .signWith(SignatureAlgorithm.HS256, key)
+                .claim("id", id) // user table PK
+                // Signature 설정 : secret key를 활용한 암호화.
+                .signWith(key, SignatureAlgorithm.HS256)
                 .setExpiration(expiry)
                 .compact();
     }
 
-//    public boolean validate() {
-//        log.debug("validate");
-//        return this.getTokenClaims() != null;
-//    }
+    public boolean validate() {
+        log.debug("validate");
+        return this.getTokenClaims() != null;
+    }
 
     /*
     토큰 뜯어보기
      */
-//    public Claims getTokenClaims() {
-//        log.debug("getTokenClaims");
-//        log.debug("key = {}", key);
-//        try {
-//            return Jwts.parserBuilder()
-//                    .setSigningKey(key)
-//                    .build()
-//                    .parseClaimsJws(token)
-//                    .getBody();
-//        } catch (SecurityException e) {
-//            log.info("Invalid JWT signature.");
-//        } //catch (MalformedJwtException e) {
-////            log.info("Invalid JWT token.");
-////        } catch (ExpiredJwtException e) {
-////            log.info("Expired JWT token.");
-////        } catch (UnsupportedJwtException e) {
-////            log.info("Unsupported JWT token.");
-////        } catch (IllegalArgumentException e) {
-////            log.info("JWT token compact of handler are invalid.");
-////            log.debug(e.getMessage());
-////        }
-//        return null;
-//    }
-//
-//    public Claims getExpiredTokenClaims() {
-//        try {
-//            parserBuilder()
-//                    .setSigningKey(key)
-//                    .build()
-//                    .parseClaimsJws(token)
-//                    .getBody();
-//        } catch (ExpiredJwtException e) {
-////            log.info("Expired JWT token");
-////            return e.getClaims();
-////        }
-//            return null;
-//        }
-//    }
+    public Claims getTokenClaims() {
+        log.debug("getTokenClaims");
+        log.debug("key = {}", key);
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (SecurityException e) {
+            log.info("Invalid JWT signature.");
+        } catch (MalformedJwtException e) {
+            log.info("Invalid JWT token.");
+        } catch (ExpiredJwtException e) {
+            log.info("Expired JWT token.");
+        } catch (UnsupportedJwtException e) {
+            log.info("Unsupported JWT token.");
+        } catch (IllegalArgumentException e) {
+            log.info("JWT token compact of handler are invalid.");
+            log.debug(e.getMessage());
+        }
+        return null;
+    }
+
+    public Claims getExpiredTokenClaims() {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (ExpiredJwtException e) {
+            log.info("Expired JWT token");
+            return e.getClaims();
+        }
+        return null;
+
+    }
 }
--- backend/src/main/java/com/bbb/songeoreum/jwt/AuthTokenProvider.java
+++ backend/src/main/java/com/bbb/songeoreum/jwt/AuthTokenProvider.java
@@ -1,13 +1,22 @@
 package com.bbb.songeoreum.jwt;
 
 import com.bbb.songeoreum.db.repository.UserRepository;
+import com.bbb.songeoreum.exception.TokenValidFailedException;
+import com.bbb.songeoreum.oauth.entity.PrincipalDetails;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -15,29 +24,29 @@ public class AuthTokenProvider {
     private final Key key;
     private static final String AUTHORITIES_KEY = "role";
 
-
-//    private final UserRepository userRepository;
+    // 보통 private final을 붙여야 하는데 이건 왜 final을 지워야 에러가 안 나는건지 이해가 안 됨.
+    private UserRepository userRepository;
 
     /**
      * 객체 초기화
      *
-     * @param secrete: jwt의 secrete
+     * @param secret: jwt의 secret
      */
-    public AuthTokenProvider(String secrete) {
-        this.key = Keys.hmacShaKeyFor(secrete.getBytes());
+    public AuthTokenProvider(String secret) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
     }
 
     /*
     jwt refresh 토큰 생성
      */
-    public AuthToken createAuthToken(String id, Date expiry) {
+    public AuthToken createAuthToken(Long id, Date expiry) {
         return new AuthToken(id, expiry, key);
     }
 
     /*
     jwt access 토큰 생성
      */
-    public AuthToken createAuthToken(String id, String role, Date expiry) {
+    public AuthToken createAuthToken(Long id, String role, Date expiry) {
         return new AuthToken(id, role, expiry, key);
     }
 
@@ -45,25 +54,25 @@ public class AuthTokenProvider {
         return new AuthToken(token, key);
     }
 
-//    public Authentication getAuthentication(AuthToken authToken) {
-        // 토큰 검증
-//        if (authToken.validate()) {
-//
-//            // claims 가져오기
-//            Claims claims = authToken.getTokenClaims();
-//            Collection<? extends GrantedAuthority> authorities =
-//                    // claims 중에 role 꺼내서 가져오기 == 권한 가져오기
-//                    Arrays.stream(new String[]{claims.get(AUTHORITIES_KEY).toString()})
-//                            .map(SimpleGrantedAuthority::new)
-//                            .collect(Collectors.toList());
-//
-//            log.debug("claims subject := [{}]", claims.getSubject()); // 이메일 반환
-//            // 시큐리티 인증 객체 가져오기
-//            PrincipalDetails principalDetails = new PrincipalDetails(userRepository.findByEmail(claims.getSubject()).get());
-//            return new UsernamePasswordAuthenticationToken(principalDetails, authToken, authorities);
-//        } else {
-//            throw new TokenValidFailedException();
-//        }
-//    }
+    public Authentication getAuthentication(AuthToken authToken) {
+//         토큰 검증
+        if (authToken.validate()) {
+
+            // claims 가져오기
+            Claims claims = authToken.getTokenClaims();
+            Collection<? extends GrantedAuthority> authorities =
+                    // claims 중에 role 꺼내서 가져오기 == 권한 가져오기
+                    Arrays.stream(new String[]{claims.get(AUTHORITIES_KEY).toString()})
+                            .map(SimpleGrantedAuthority::new)
+                            .collect(Collectors.toList());
+
+            log.debug("claims subject := [{}]", claims.getSubject()); // user table pk 반환
+            // 시큐리티 인증 객체 가져오기
+            PrincipalDetails principalDetails = new PrincipalDetails(userRepository.findByEmail(claims.getSubject()).get());
+            return new UsernamePasswordAuthenticationToken(principalDetails, authToken, authorities);
+        } else {
+            throw new TokenValidFailedException();
+        }
+    }
 
 }
