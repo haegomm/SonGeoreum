6041c3901720c177c2ba1c57b975d94f12809e88
jeongjuhwang <hjj950303@naver.com>
2023-02-06 16:49:27 +0900

    fix: fix bug so that jwt can be issued after login

--- backend/src/main/java/com/bbb/songeoreum/oauth/handler/OAuth2AuthenticationSuccessHandler.java
+++ backend/src/main/java/com/bbb/songeoreum/oauth/handler/OAuth2AuthenticationSuccessHandler.java
@@ -1,18 +1,23 @@
 package com.bbb.songeoreum.oauth.handler;
 
+import com.bbb.songeoreum.api.service.UserService;
 import com.bbb.songeoreum.config.AppProperties;
 import com.bbb.songeoreum.db.domain.User;
 import com.bbb.songeoreum.db.repository.OAuth2AuthorizationRequestBasedOnCookieRepository;
 import com.bbb.songeoreum.db.repository.UserRepository;
 import com.bbb.songeoreum.jwt.AuthToken;
 import com.bbb.songeoreum.jwt.AuthTokenProvider;
+import com.bbb.songeoreum.oauth.entity.PrincipalDetails;
 import com.bbb.songeoreum.oauth.entity.ProviderType;
 import com.bbb.songeoreum.oauth.entity.RoleType;
 import com.bbb.songeoreum.oauth.info.OAuth2UserInfo;
 import com.bbb.songeoreum.oauth.info.OAuth2UserInfoFactory;
 import com.bbb.songeoreum.util.CookieUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
@@ -22,10 +27,13 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.transaction.Transactional;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.Collection;
@@ -43,7 +51,6 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
     private final UserRepository userRepository;
     private final AuthTokenProvider tokenProvider;
     private final AppProperties appProperties;
-    //    private final UserRefreshTokenRepository userRefreshTokenRepository;
     private final OAuth2AuthorizationRequestBasedOnCookieRepository authorizationRequestRepository;
 
     @Override
@@ -70,37 +77,26 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
             throw new IllegalArgumentException("비인가 Redirect URI를 받아 인증을 진행할 수 없습니다!");
         }
 
-        String targetUrl = redirectUri.orElse(getDefaultTargetUrl());
+        String targetUrl = redirectUri.orElse(getDefaultTargetUrl()); // "/" 가 들어가 있음.
         log.debug("determineTargetUrl method targetUrl : {}", targetUrl);
 
-        // OAuth(소셜로그인)로 인증된 사용자는 OAuth2AuthenticationToken 유형이다.
-        OAuth2AuthenticationToken authToken = (OAuth2AuthenticationToken) authentication;
-        log.debug("authToken : {}", authToken);
-        ProviderType providerType = ProviderType.valueOf(authToken.getAuthorizedClientRegistrationId().toUpperCase());
-
+        log.debug("authentication.getPrincipal() : {}", authentication.getPrincipal().toString());
         // Authentication 객체 안에는 UserDetails 타입(일반사용자)과 OAuth2User 타입(카카오사용자)만 저장할 수 있다.
-        OidcUser oidcUser = ((OidcUser) authentication.getPrincipal()); // 현재 Authentication 객체에 들어있는 사용자 객체를 반환
-        OAuth2UserInfo userInfo = OAuth2UserInfoFactory.getOAuth2UserInfo(providerType, oidcUser.getAttributes());
-        Collection<? extends GrantedAuthority> authorities = ((OidcUser) authentication.getPrincipal()).getAuthorities();
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal(); // 현재 Authentication 객체에 들어있는 사용자 객체를 반환
+        log.debug("principalDetails : {}", principalDetails);
+        User user = principalDetails.getUser(); // 카카오톡 사용자 정보 가져옴.
+        log.debug("userInfo : {}", user);
+        Collection<? extends GrantedAuthority> authorities = ((PrincipalDetails) authentication.getPrincipal()).getAuthorities();
 
         // 토큰 저장 시작
         RoleType roleType = hasAuthority(authorities, RoleType.ADMIN.getCode()) ? RoleType.ADMIN : RoleType.USER;
 
         Date now = new Date();
 
-        User user = userRepository.findByKakaoId(userInfo.getProviderId());
-        log.debug("determineTargetUrl 메서드에서 user 조회 결과 : {}", user);
-
-        Long id = 0L; // user 테이블의 pk 저장해줄 변수
-        if (user == null) {
-            id = userRepository.count() + 1;
-        } else {
-            id = user.getId();
-        }
 
         // access 토큰 설정
         AuthToken accessToken = tokenProvider.createAuthToken(
-                id, // access 토큰에 user pk 저장
+                user.getId(), // access 토큰에 user pk 저장
                 roleType.getCode(),
                 new Date(now.getTime() + appProperties.getAuth().getTokenExpiry())
         );
@@ -114,26 +110,22 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
                 new Date(now.getTime() + refreshTokenExpiry)
         );
 
+        log.debug("accessToken : {}", accessToken.getToken());
+        log.debug("refreshToken : {}", refreshToken.getToken());
+
         // DB 저장
-//        User userRefreshToken = userRepository.findByKakaoId(userInfo.getProviderId());
-        // 카카오 아이디로 조회 결과 가입한 적 있는 경우
-        if (user != null) {
-            user.saveRefreshToken(refreshToken.getToken());
-        }
-        // 카카오 아이디로 조회 결과 가입한 적 없는 경우
-        else {
-            LocalDateTime createdDate = LocalDateTime.now();
-            String nickname = "guest" + (userRepository.count() + 1);
-            user = new User(providerType.toString(), userInfo.getEmail(), userInfo.getProviderId(), nickname, refreshToken.getToken(), createdDate);
-            userRepository.saveAndFlush(user); // save() 메서드와 달리 실행중(트랜잭션)에 즉시 data를 flush 함.
-        }
+        user.saveRefreshToken(refreshToken.getToken());
+        log.debug("userInfo 리프레시 토큰 저장한 후 : {}", user.getRefreshToken());
+        userRepository.saveAndFlush(user); // save() 메서드와 달리 실행중(트랜잭션)에 즉시 data를 flush 함.
 
         int cookieMaxAge = (int) refreshTokenExpiry / 60;
 
         CookieUtil.deleteCookie(request, response, REFRESH_TOKEN);
         CookieUtil.addCookie(response, REFRESH_TOKEN, refreshToken.getToken(), cookieMaxAge);
 
-        return UriComponentsBuilder.fromUriString(targetUrl)
+//        response.addHeader("accessToken", accessToken.getToken());
+
+        return UriComponentsBuilder.fromUriString(targetUrl + "oauth2/code/kakao")
                 .queryParam("token", accessToken.getToken())
                 .build().toUriString();
     }
