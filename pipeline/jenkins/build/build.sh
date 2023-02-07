2d35154d95f9132d0493cbcff3f7c198361e3a65
Youngmook Lim <youngmookk@gmail.com>
2023-02-08 02:31:32 +0900

    feat: add print messages for each stage of build, deploy

--- pipeline/jenkins/build/build.sh
+++ pipeline/jenkins/build/build.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+echo "*******************************"
+echo "*******Building FRONTEND*******"
+echo "*******************************"
+
 # frontend build
 cd /var/jenkins_home/workspace/bbb-pipeline/frontend
 npm install
 npm run build
 
+echo "********************************"
+echo "********Building BACKEND********"
+echo "********************************"
+
 # backend build
 cd /var/jenkins_home/workspace/bbb-pipeline/backend
 gradle clean build -x test
+
+echo "********************************"
+echo "***Building DOCKER CONTAINERS***"
+echo "********************************"
 
 # docker container build
 cd /var/jenkins_home/workspace/bbb-pipeline/pipeline
--- pipeline/jenkins/deploy/deploy.sh
+++ pipeline/jenkins/deploy/deploy.sh
@@ -1,3 +1,7 @@
+echo "*********************************"
+echo "***DEPLOYING DOCKER CONTAINERS***"
+echo "*********************************"
+
 # docker container run
 cd /var/jenkins_home/workspace/bbb-pipeline/pipeline
 docker compose up -d
