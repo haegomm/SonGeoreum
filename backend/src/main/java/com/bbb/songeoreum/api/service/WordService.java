c264f4a3fa05483a09b93f16d1c6e4820633c46e
sonmh79 <sonmh79@naver.com>
2023-02-08 14:33:33 +0900

    fix: fix service logic to filter by request parameter

--- backend/src/main/java/com/bbb/songeoreum/api/service/WordService.java
+++ backend/src/main/java/com/bbb/songeoreum/api/service/WordService.java
@@ -3,10 +3,13 @@ package com.bbb.songeoreum.api.service;
 import com.bbb.songeoreum.api.response.WordRes;
 import com.bbb.songeoreum.db.domain.Word;
 import com.bbb.songeoreum.db.repository.WordRepository;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,13 +33,42 @@ public class WordService {
         return wordResList;
     }
 
-    public List<WordRes> findAllWords() {
+//    public List<WordRes> findAllWords() {
+//
+//        List<Word> words = wordRepository.findAll();
+//
+//        log.debug("find all words: {}", words);
+//
+//        List<WordRes> wordResList = words.stream().map(word -> WordRes.builder().word(word).build()).collect(Collectors.toList());
+//
+//        return wordResList;
+//    }
 
-        List<Word> words = wordRepository.findAll();
+    public List<WordRes> findAllWords(Long categoryId, Boolean isRandom, Boolean isTestable, Integer num) {
 
-        log.debug("find all words: {}", words);
+        List<Word> findWords;
 
-        List<WordRes> wordResList = words.stream().map(word -> WordRes.builder().word(word).build()).collect(Collectors.toList());
+        if (categoryId != null) {
+            findWords = wordRepository.findByCategory_Id(categoryId);
+        } else {
+            findWords = wordRepository.findAll();
+        }
+
+        if (isRandom != null && isRandom) {
+            Collections.shuffle(findWords);
+        }
+
+        if (isTestable != null && !isTestable) {
+            findWords = findWords.stream().filter(word -> word.getCategory().getIsTestable() == 'F').collect(Collectors.toList());
+        }
+
+        if (num != null) {
+            findWords = findWords.stream().limit(num).collect(Collectors.toList());
+        }
+
+        log.debug("find find words: {}", findWords);
+
+        List<WordRes> wordResList = findWords.stream().map(word -> WordRes.builder().word(word).build()).collect(Collectors.toList());
 
         return wordResList;
     }
