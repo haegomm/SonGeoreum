ae8c8b21321891d3961a2ab12b2c276f554b1b3e
황정주 <hjj950303@naver.com>
2023-02-09 22:41:03 +0900

    feat: add files for getting access token from Kakao

---
+++ backend/src/main/java/com/bbb/songeoreum/api/service/AuthService.java
@@ -0,0 +1,81 @@
+package com.bbb.songeoreum.api.service;
+
+import com.bbb.songeoreum.oauth.info.KakaoTokenDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)  // 트랜잭션 안에서만 데이터 변경하게 설정
+public class AuthService {
+
+    @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
+    private String KAKAO_CLIENT_ID;
+
+    @Value("${spring.security.oauth2.client.registration.kakao.client-secret}")
+    private String KAKAO_CLIENT_SECRET;
+
+    @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
+    private String KAKAO_REDIRECT_URI;
+
+    @Value("${spring.security.oauth2.client.provider.kakao.token-uri}")
+    private String KAKAO_TOKEN_URI;
+
+    // 인가코드로 kakaoAccessToken 따오는 메소드
+    public KakaoTokenDto getKakaoAccessToken(String code) {
+
+        RestTemplate rt = new RestTemplate(); //통신용
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        // HttpBody 객체 생성
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code"); //카카오 공식문서 기준 authorization_code 로 고정
+        params.add("client_id", KAKAO_CLIENT_ID); //카카오 앱 REST API 키
+        params.add("client_secret", KAKAO_CLIENT_SECRET);
+        params.add("redirect_uri", KAKAO_REDIRECT_URI);
+        params.add("code", code); //인가 코드 요청시 받은 인가 코드값, 프론트에서 받아오는 그 코드
+        log.debug("인가 코드 넘어옴 : {}", params.get("code"));
+
+        // 헤더와 바디 합치기 위해 HttpEntity 객체 생성
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
+        log.debug("kakaoTokenRequest : {}", kakaoTokenRequest);
+
+        // 카카오로부터 Access token 수신
+        ResponseEntity<String> accessTokenResponse = rt.exchange(
+                KAKAO_TOKEN_URI,
+                HttpMethod.POST,
+                kakaoTokenRequest,
+                String.class
+        );
+
+        log.debug("accessTokenResponse : {} ", accessTokenResponse);
+
+        // JSON Parsing (-> KakaoTokenDto)
+        ObjectMapper objectMapper = new ObjectMapper();
+        KakaoTokenDto kakaoTokenDto = null;
+        try {
+            kakaoTokenDto = objectMapper.readValue(accessTokenResponse.getBody(), KakaoTokenDto.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return kakaoTokenDto;
+    }
+
+
+}
