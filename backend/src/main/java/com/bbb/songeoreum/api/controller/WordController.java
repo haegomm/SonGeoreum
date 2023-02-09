2070c2ea661c30c47be759cda8940232da2feaab
sonmh79 <sonmh79@naver.com>
2023-02-09 13:30:30 +0900

    feat: add comment to word controller

--- backend/src/main/java/com/bbb/songeoreum/api/controller/WordController.java
+++ backend/src/main/java/com/bbb/songeoreum/api/controller/WordController.java
@@ -13,15 +13,28 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * {@code WordController}는 단어에 관련된 API를 처리하는 컨트롤러입니다.
+ *
+ * @author sonmh79
+ * @version 1.0
+ */
 @Slf4j
 @RestController
 @RequestMapping("/api/words")
 @RequiredArgsConstructor
 @Api(tags = {"단어 API"}) // Swagger에 보여줄 명칭
 public class WordController {
-
     private final WordService wordService;
 
+    /**
+     * 특정 조건을 만족하는 단어들을 반환합니다
+     * @param categoryId 카테고리의 PK.
+     * @param isRandom 랜덤으로 순서를 섞을지 여부 (true/false).
+     * @param isTestable 테스트 가능한지 여부 (true/false).
+     * @param num 응답 시 원하는 단어의 개수.
+     * @return 단어 리스트를 {@code ResponseEntity}로 반환합니다.
+     */
     @GetMapping
     @ApiOperation("특정 조건을 만족하는 모든 단어를 리스트에 담아 응답한다.")
     public ResponseEntity<List<WordRes>> findWords(
