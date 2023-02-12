faddd66b32a2d1255a71b2705a1705430f3c7260
황정주 <hjj950303@naver.com>
2023-02-12 16:33:11 +0900

    refactor: modify nickname max size from 6 to 10 in InsertUserReq class

--- backend/src/main/java/com/bbb/songeoreum/api/request/InsertUserReq.java
+++ backend/src/main/java/com/bbb/songeoreum/api/request/InsertUserReq.java
@@ -31,7 +31,7 @@ public class InsertUserReq {
 
     // 닉네임
     @NotBlank // Null, 빈 문자열, 스페이스만 있는 문자열 불가
-    @Size(min = 2, max = 6)
+    @Size(min = 2, max = 10)
     private String nickname;
 
     // 프로필 사진 URL
