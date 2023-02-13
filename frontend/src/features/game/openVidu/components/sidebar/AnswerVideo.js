2fa39c10e6c91b0d7771cff884a5e1e0fa5dd6ee
leegaeun <rkdms4054@gmail.com>
2023-02-13 23:35:36 +0900

    refactor: answer word in timer

--- frontend/src/features/game/openVidu/components/sidebar/AnswerVideo.js
+++ frontend/src/features/game/openVidu/components/sidebar/AnswerVideo.js
@@ -1,38 +1,38 @@
 import React, { useEffect, useState } from "react";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import lock from "../../assets/images/lock.jpg";
-import "./Timer.scss"
-
-const renderTime = ({ remainingTime }) => {
-  if (remainingTime === 0) {
-    return <div className="timer">정답을 시청해주세요</div>
-  }
-
-  return (
-    <div className="timer">
-      <div className="value">{remainingTime}</div>
-    </div>
-  );
-};
+import "./Timer.scss";
 
 const AnswerVideo = (props) => {
-  console.log("리렌더링!!!", props.showAnswer)
+  console.log("리렌더링!!!", props.showAnswer);
   const myNickname = props.myNickname;
   const answerWord = props.answerWord;
   const answerApi = props.answerApi;
   const presenter = props.presenter;
   const showAnswer = props.showAnswer;
-    
-    useEffect(() => {
-      if (!showAnswer) {
-        setTimeout(() => {
-          props.whoGetScore('')
-        }, 5000)
-        }
-    }, [])
-    
-    
-  const check = presenter === myNickname ? "내가 출제자야" : (`다음 출제자: ${presenter}`);
+
+  const renderTime = ({ remainingTime }) => {
+    if (remainingTime === 0) {
+      return <div className="timer">{answerWord}</div>;
+    }
+
+    return (
+      <div className="timer">
+        <div className="value">{remainingTime}</div>
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    if (!showAnswer) {
+      setTimeout(() => {
+        props.whoGetScore("");
+      }, 5000);
+    }
+  }, []);
+
+  const check =
+    presenter === myNickname ? "내가 출제자야" : `다음 출제자: ${presenter}`;
   return (
     <div>
       <div className="timer-wrapper">
@@ -48,14 +48,12 @@ const AnswerVideo = (props) => {
       <div className="box">
         {props.showAnswer || presenter === myNickname ? (
           <div className="box">
-            <div className="box">{answerWord} || {check}</div>
             <video autoPlay loop>
               <source src={answerApi}></source>
             </video>
           </div>
         ) : (
           <div className="box">
-            <div className="box">무엇일까요?</div>
             <img className="box" src={lock}></img>
           </div>
         )}
