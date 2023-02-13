6be86222774fb2afe031a6fb9203077d436241e0
Youngmook Lim <youngmookk@gmail.com>
2023-02-13 14:44:22 +0900

    feat: style loading screen

--- frontend/src/features/game/openVidu/test.js
+++ frontend/src/features/game/openVidu/test.js
@@ -3,10 +3,10 @@ import VideoRoomComponent from "./components/VideoRoomComponent";
 function Game() {
   return (
     <div>
-      <button>입장하기</button>
+      {/* <button>입장하기</button> */}
       <VideoRoomComponent />;
     </div>
-  )
+  );
 }
 
 export default Game;
