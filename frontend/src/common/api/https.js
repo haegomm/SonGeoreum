198777272284274c81bc550f53bf0a8afcaf3bd4
Youngmook Lim <youngmookk@gmail.com>
2023-02-15 01:02:44 +0900

    refactor: temporary disable of axios interceptor for css work

--- frontend/src/common/api/https.js
+++ frontend/src/common/api/https.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import baseAxios from 'axios';
-import { deleteUserInfo, getUserInfo } from './authInfo';
+import React from "react";
+import baseAxios from "axios";
+import { deleteUserInfo, getUserInfo } from "./authInfo";
 
 const axios = baseAxios.create({
-  baseURL: process.env.REACT_APP_API,     // 환경변수 세팅
+  baseURL: process.env.REACT_APP_API, // 환경변수 세팅
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
@@ -15,24 +15,24 @@ axios.interceptors.request.use((config) => {
   return config;
 });
 
-axios.interceptors.response.use(
-  function (response) {
-    return response;
-  },
-  function (error) {
-    if (error.response && error.response.status) {
-      switch (error.response.status) {
-        case 401:
-          alert('로그인이 필요합니다');
-            window.location.replace("/login")
-            deleteUserInfo()
-            return new Promise(() => {});
-        default:
-          return Promise.reject(error);
-      }
-    }
-    return Promise.reject(error);
-  },
-);
-  
+// axios.interceptors.response.use( // ***** 임시 *****
+//   function (response) {
+//     return response;
+//   },
+//   function (error) {
+//     if (error.response && error.response.status) {
+//       switch (error.response.status) {
+//         case 401:
+//           alert('로그인이 필요합니다');
+//             window.location.replace("/login")
+//             deleteUserInfo()
+//             return new Promise(() => {});
+//         default:
+//           return Promise.reject(error);
+//       }
+//     }
+//     return Promise.reject(error);
+//   },
+// );
+
 export default axios;
