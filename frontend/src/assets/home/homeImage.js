1f2e432d8bb81ab8ee651361cef401d0952ea7e4
seohyeonkyeong <hkyung99@naver.com>
2023-02-16 00:39:26 +0900

    fix: add comment for no file

--- frontend/src/assets/home/homeImage.js
+++ frontend/src/assets/home/homeImage.js
@@ -1,15 +1,15 @@
-import homeImage0 from './0.png';
-import homeImage1 from './1.png';
-import homeImage2 from './2.png';
-import homeImage3 from './3.png';
-import homeImage4 from './1-1.png';
+import homeImage0 from "./0.png";
+import homeImage1 from "./1.png";
+import homeImage2 from "./2.png";
+import homeImage3 from "./3.png";
+// import homeImage4 from './1-1.png';
 
 const homeImages = [
   homeImage0,
   homeImage1,
   homeImage2,
   homeImage3,
-  homeImage4,
+  // homeImage4,
 ];
 
 export default homeImages;
