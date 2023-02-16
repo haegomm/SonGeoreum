6f2ed993e6c9618f2218417b28e32fb20eb3b996
seohyeonkyeong <hkyung99@naver.com>
2023-02-17 01:47:57 +0900

    refactor: api http code refactor

--- frontend/src/common/api/https.js
+++ frontend/src/common/api/https.js
@@ -1,15 +1,13 @@
-import React from "react";
 import baseAxios from "axios";
 import { deleteUserInfo, getUserInfo } from "./authInfo";
 
 const axios = baseAxios.create({
-  baseURL: process.env.REACT_APP_API, // 환경변수 세팅
+  baseURL: process.env.REACT_APP_API,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// request 보내기 전에 인터셉트 해서 이 작업을 선행한다: 헤더에 토큰 넣기
 axios.interceptors.request.use((config) => {
   config.headers.Authorization = `Bearer ${getUserInfo().accessToken}`;
   return config;
@@ -23,7 +21,7 @@ axios.interceptors.response.use(
     if (error.response && error.response.status) {
       switch (error.response.status) {
         case 401:
-          alert("로그인이 필요합니다");
+          alert("로그인이 필요합니다.");
           window.location.replace("/login");
           deleteUserInfo();
           return new Promise(() => {});
