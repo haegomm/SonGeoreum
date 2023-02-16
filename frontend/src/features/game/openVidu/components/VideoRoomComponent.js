6bbf81f33b262010ff4f9034bf2f7d872624319f
seohyeonkyeong <hkyung99@naver.com>
2023-02-16 10:42:54 +0900

    fix: go game test code modify

--- frontend/src/features/game/openVidu/components/VideoRoomComponent.js
+++ frontend/src/features/game/openVidu/components/VideoRoomComponent.js
@@ -43,7 +43,7 @@ class VideoRoomComponent extends Component {
       sessionId: undefined, //
       token: "", //
       playGame: false, //
-      goGame: true, // *** 임시 ***
+      goGame: false, //
       playersList: null, //
       wordsList: null,
       subToken: undefined, // ?
